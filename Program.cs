a155704f27a89ded633c45743f95b334b0af9c14
Lenovo-User <Lenovo-User@DESKTOP-Q92463H>
2023-10-09 12:11:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Getting_User_input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name);
+            Console.Write("How old are you");
+            string age = Console.ReadLine();
+            Console.WriteLine("you are " + age);
+            Console.ReadLine();
+        }
+    }
+}
